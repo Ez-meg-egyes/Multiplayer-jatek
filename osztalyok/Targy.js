7f754272f6a58dd24abc6aeda099dfc6273ad758
KSZBence <kunszekelybence@gmil.com>
2023-05-19 10:25:15 +0200

    targy getterek és constructor

--- osztalyok/Targy.js
+++ osztalyok/Targy.js
@@ -1,8 +1,25 @@
 
 class Targy {
-    
-    constructor() {
-        
+    #nev
+    #kep
+    #tulajdonsag
+    #divElem
+    constructor(nev, kep, tulajdonsag) {
+        this.#nev = nev
+        this.#kep = kep
+        this.#tulajdonsag = tulajdonsag
+    }
+
+    getNev(){
+        return this.#nev
+    }
+
+    getKep(){
+        return this.#kep
+    }
+
+    getTulajdonsag(){
+        return this.#tulajdonsag
     }
 }
 
