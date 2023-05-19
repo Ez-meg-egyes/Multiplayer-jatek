0a5e77df243bcc12758348a90567a0d19530f00c
Péti Dominik (SZF_2022_2024) <peti.dominik@diak.szamalk-szalezi.hu>
2023-05-19 10:31:55 +0200

    Karakter, Jatekos, Ellenseg osztály UML ábra alapján létrehozva

--- osztalyok/Karakter.js
+++ osztalyok/Karakter.js
@@ -1,9 +1,52 @@
 
 
 class Karakter {
+    #eletero;
+    #sebzes;
+    #pos;
+    #kep;
+    #divElem;
+    #szuloElem;
     
-    constructor() {
+    constructor(eletero, sebzes, pos, kep, szuloElem) {
+        this.setEletero(eletero);
+        this.setSebzes(sebzes);
+        this.setPos(pos);
+        this.#kep = kep;
+        this.#szuloElem = szuloElem;
+    }
 
+    getEletero() {
+        return this.#eletero;
+    }
+
+    getSebzes() {
+        return this.#sebzes;
+    }
+
+    getPos() {
+        return this.#pos;
+    }
+
+    setEletero(ujEletero) {
+        this.#eletero = ujEletero;
+    }
+
+    setSebzes(ujSebzes) {
+        this.#sebzes = ujSebzes;
+    }
+
+    setPos(ujPos) {
+        this.pos = ujPos;
+
+    }
+
+    modositEletero(modosito) {
+        this.setEletero(this.#eletero + modosito);
+    }
+
+    modositSebzes(modosito) {
+        this.setSebzes(this.#sebzes + modosito);
     }
 }
 
