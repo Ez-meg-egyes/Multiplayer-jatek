306d34a50c721afac89217bbfd9492008f9dface
Péti Dominik (SZF_2022_2024) <peti.dominik@diak.szamalk-szalezi.hu>
2023-05-19 10:53:04 +0200

    tagfüggvények, adattagok hozzáadva

--- osztalyok/Ellenseg.js
+++ osztalyok/Ellenseg.js
@@ -3,12 +3,16 @@ import Karakter from "./Karakter.js";
 class Ellenseg extends Karakter {
     #dobottTargy;
 
-    constructor(eletero, sebzes, pos, kep, szuloElem) {
-        super(eletero, sebzes, pos, kep, szuloElem);
+    constructor(id, eletero, sebzes, pos, kep, szuloElem) {
+        super(id, eletero, sebzes, pos, kep, szuloElem);
     }
 
     getDobottTargy() {
         return this.#dobottTargy;
+    }
+
+    #htmlBeagyazas(szuloElem) {
+        szuloElem.append(`<div id="ellenseg-${this.getId()}" class="karakter"></div>`)
     }
 }
 
