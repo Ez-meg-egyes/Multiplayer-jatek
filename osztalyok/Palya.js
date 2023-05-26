4de02e46bb3f3a845224b612fd75d33b27045e71
Péti Dominik (SZF_2022_2024) <peti.dominik@diak.szamalk-szalezi.hu>
2023-05-26 09:05:24 +0200

    Palya : pályaelemek eltárolva

--- osztalyok/Palya.js
+++ osztalyok/Palya.js
@@ -23,11 +23,13 @@ class Palya {
 
     palyaInit(palyaMeret){        
         for (let i = 0; i < palyaMeret[1]; i++) {
+            this.#palyaElemek[i] = [];
+
             for (let j = 0; j < palyaMeret[0]; j++) {
                 if ((i == 0 || i == palyaMeret[1]-1) || (j == 0 || j == palyaMeret[0]-1)) {
-                    let DIV = new PalyaElem("fal");
+                    this.#palyaElemek[i].push(new PalyaElem("fal"));
                 } else {
-                    let DIV = new PalyaElem("talaj");
+                    this.#palyaElemek[i].push(new PalyaElem("talaj"));
                 }
             }
         }
